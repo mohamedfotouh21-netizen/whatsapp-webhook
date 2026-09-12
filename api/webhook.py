139181bce74cd4dfc607a20ee420a57481084439
mohamedfotouh21-netizen <mohamed.fotouh21@gmail.com>
2026-09-12 14:27:32 +0300

    Create webhook.py

---
+++ api/webhook.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "koshri_webhook_2026"
+
+@app.route("/api/webhook", methods=["GET"])
+def verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+
+    return "Forbidden", 403
+
+
+@app.route("/api/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    print("WEBHOOK:")
+    print(data)
+
+    return "OK", 200
